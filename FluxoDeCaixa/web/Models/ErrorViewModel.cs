c452b0d3968be4d2ebfcfaae630744dbfbc924e1
Vinicius Gasparello da Silva <gasparellovinicius@gmail.com>
2024-12-02 15:28:56 -0300

    web app chenges

--- FluxoDeCaixa/web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FluxoDeCaixa.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ FluxoDeCaixa/web/SFC.Host/Components/Pages/Wallet.razor.js
@@ -0,0 +1,5 @@
+export class Wallet {
+  
+}
+
+window.Wallet = Wallet;
